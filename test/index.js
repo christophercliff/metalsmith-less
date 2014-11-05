10d8b858c38df4d55a4242a33e295e48300e8099
Christopher Cliff <chris@percolate.com>
2014-11-05 09:52:42 -0500

    Added jscs.

--- test/index.js
+++ test/index.js
@@ -2,19 +2,19 @@ var assertDir = require('assert-dir-equal')
 var less = require('../')
 var Metalsmith = require('metalsmith')
 
-describe('metalsmith-less', function(){
+describe('metalsmith-less', function () {
 
-    it('should convert less to css', function(done){
+    it('should convert less to css', function (done) {
         (new Metalsmith('test/fixtures/basic'))
             .use(less())
-            .build(function(err){
+            .build(function (err) {
                 if (err) return done(err)
                 assertDir('test/fixtures/basic/expected', 'test/fixtures/basic/build')
                 return done(null)
         })
     })
 
-    it('should convert imported files', function(done){
+    it('should convert imported files', function (done) {
         (new Metalsmith('test/fixtures/import'))
             .use(less({
                 pattern: 'less/index.less',
@@ -22,14 +22,14 @@ describe('metalsmith-less', function(){
                     paths: ['test/fixtures/import/src/less']
                 }
             }))
-            .build(function(err){
+            .build(function (err) {
                 if (err) return done(err)
                 assertDir('test/fixtures/import/expected', 'test/fixtures/import/build')
                 return done(null)
         })
     })
 
-    it('should create source map', function(done){
+    it('should create source map', function (done) {
         (new Metalsmith('test/fixtures/source-map'))
             .use(less({
                 pattern: 'less/index.less',
@@ -38,7 +38,7 @@ describe('metalsmith-less', function(){
                     paths: ['test/fixtures/source-map/src/less']
                 }
             }))
-            .build(function(err){
+            .build(function (err) {
                 if (err) return done(err)
                 assertDir('test/fixtures/source-map/expected', 'test/fixtures/source-map/build')
                 return done(null)
